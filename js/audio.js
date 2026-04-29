055d065e35e7d810fb9dd7000e4069735ecd073c
EGY-UAE <egy.uae.eg@gmail.com>
2026-04-29 22:39:19 +0300

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,8 +1,8 @@
 document.addEventListener('click', function() {
     var audio = document.getElementById("myAudio");
     
-    //  (20%)
-    audio.volume = 0.2; 
+    //  (60%)
+    audio.volume = 0.6; 
     
     audio.play().catch(function(error) {
         console.log("error, we are sorry:", error);
